5af01b84d473c01d997b0403f70a5db0c9ec0281
adam4521 <akjaworski@gmail.com>
2019-11-12 14:55:48 +0000

    removed blinkt.py

--- blinkt_indicator.py
+++ blinkt_indicator.py
@@ -13,15 +13,14 @@ colours = { 'red': (200,0,0), 'green': (0,200,0), 'blue': (0,0,200), 'yellow': (
         'cyan': (0,200,200), 'magenta': (200,0,200), 'white': (200,200,200), 'off': (0,0,0) }
 
 
-if __name__ == '__main__':
-    try:
-        col = colours[sys.argv[2]]
-        blinkt.set_clear_on_exit(False)
-        blinkt.set_pixel(int(sys.argv[1]), col[0], col[1], col[2])
-        blinkt.show()
-    except:
-        sys.stderr.write('Colour or pixel not recognised.\n')
-        sys.exit(1)
+try:
+    col = colours[sys.argv[2]]
+    blinkt.set_clear_on_exit(False)
+    blinkt.set_pixel(int(sys.argv[1]), col[0], col[1], col[2])
+    blinkt.show()
+except:
+    sys.stderr.write('Colour or pixel not recognised.\n')
+    sys.exit(1)
 
 
 
